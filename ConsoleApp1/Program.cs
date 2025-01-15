87ff52aecd14c038c9e99b55e22220a02b8e9eed
DomInocencio <Dominichsinocencio@gmail.com>
2025-01-15 18:15:04 +0100

    inti commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Security;
+
+const double PI = 3.14;
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Tryk 1 for at udregne omkreds eller tryk 2 for at lukke");
+    ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+
+    Console.WriteLine();
+    Console.Write("Indtast radius");
+    Console.WriteLine();
+    double Radius = double.Parse(Console.ReadLine());
+    
+
+    if (consoleKeyInfo.KeyChar == 49) // '1'
+    {
+        Console.WriteLine("omkreds = " + (2 * Radius * PI));
+    }
+    else if(consoleKeyInfo.KeyChar == 50) // ''
+    {
+        Environment.Exit(0);
+    }
+    else
+    {
+        Console.WriteLine("areal = " + (Radius * Radius * PI));
+    }
+    Console.WriteLine("Tryk en tast for at fortsætte...");
+    Console.ReadKey();
+
+    
+}
